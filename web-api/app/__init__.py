361350a53004e12586573a95798e6e27d2387d48
Nicola Fiori (JD342) <nicola@jd342.net>
2024-03-17 18:08:09 +0000

    Rename the `html2json` module to `error2json`
    More descriptive.

--- web-api/app/__init__.py
+++ web-api/app/__init__.py
@@ -3,13 +3,13 @@ from flask import Flask
 from .data import bp as data_bp
 from .user import bp as user_bp
 from .hello import bp as hello_bp
-from .html2json import bp as html2json_bp
+from .error2json import bp as error2json_bp
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(data_bp)
     app.register_blueprint(user_bp)
     app.register_blueprint(hello_bp)
-    app.register_blueprint(html2json_bp)
+    app.register_blueprint(error2json_bp)
 
     return app
