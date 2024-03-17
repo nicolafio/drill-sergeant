68ff744d67a2b50d916607f8325b5f9bc93aea2f
Nicola Fiori (JD342) <nicola@jd342.net>
2024-03-17 14:00:15 +0000

    Remove unused import

--- web-api/app/html2json.py
+++ web-api/app/html2json.py
@@ -6,7 +6,6 @@ As Drill Sergeant is primarily a JSON-based web API, I'd like to give responses
 in JSON format whenever possible. I haven't found a better way to instruct Flask
 to give problem responses in JSON.
 """
-from http import HTTPStatus
 import json
 
 from pyquery import PyQuery as pq
