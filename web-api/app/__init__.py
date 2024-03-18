b0c3b48966cfa8effb809bd92bd72e62601f723d
Nicola Fiori (JD342) <nicola@jd342.net>
2024-03-18 03:12:44 +0000

    Remove hello endpoint

--- web-api/app/__init__.py
+++ web-api/app/__init__.py
@@ -3,7 +3,6 @@ from flask import Flask
 from .data import bp as data_bp
 from .user import bp as user_bp
 from .discipline import bp as discipline_bp
-from .hello import bp as hello_bp
 from .error2json import bp as error2json_bp
 
 def create_app():
@@ -11,7 +10,6 @@ def create_app():
     app.register_blueprint(data_bp)
     app.register_blueprint(user_bp)
     app.register_blueprint(discipline_bp)
-    app.register_blueprint(hello_bp)
     app.register_blueprint(error2json_bp)
 
     return app
